8b732f337236a836fd327ecb1e282ad90dbe1a29
Tomás Rivera <tomas.riveral@icloud.com>
2026-06-14 11:27:18 +0200

    flake.nix: init

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "ReSSPublica development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+  let
+    systems = [
+      "x86_64-linux"
+      "aarch64-linux"
+      "x86_64-darwin"
+      "aarch64-darwin"
+    ];
+
+    forAllSystems = f:
+      nixpkgs.lib.genAttrs systems (system:
+        f (import nixpkgs { inherit system; }));
+  in {
+    devShells = forAllSystems (pkgs: {
+      default = pkgs.mkShell {
+        packages = with pkgs; [
+          python3
+          python3Packages.sparqlwrapper
+          python3Packages.feedgen
+        ];
+      };
+    });
+  };
+}
