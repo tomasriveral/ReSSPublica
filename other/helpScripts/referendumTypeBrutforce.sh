6dd65cf66892082a48489b07ef1bb80445b5a378
Tomás Rivera <tomas.riveral@icloud.com>
2026-06-15 22:53:32 +0200

    other:fix helper script

--- other/helpScripts/referendumTypeBrutforce.sh
+++ other/helpScripts/referendumTypeBrutforce.sh
@@ -2,7 +2,10 @@
 # https://politics.ld.admin.ch/political-rights/referendum/typ/NUMBER # returns sometimes a referendum type. Gotta catch them all !
 echo "Until which number you want to check?"
 read number
-for i {0...number}
+
+i=0
+while [ "$i" -le "$number" ]
 do
   curl "https://politics.ld.admin.ch/political-rights/referendum/typ/$i"
+  i=$((i + 1))
 done
